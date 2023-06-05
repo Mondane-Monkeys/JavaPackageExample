356e2f09ae363269930da5b328feb5d80183a6f3
Mondane-Monkeys <65215510+Mondane-Monkeys@users.noreply.github.com>
2023-06-04 22:00:17 -0500

    Update Main.java

--- project/classa/Main.java
+++ project/classa/Main.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import project.classb.DancerCollection;
+import project.classb.dancer.Dancer;
 
 class Main {
     public static void main(String[] args) {
+        Dancer dancer = new Dancer("Tammy");
         DancerCollection dc = new DancerCollection(new ArrayList<String>(Arrays.asList(new String[]{"Jason, Jack, Mike, Marsie, Morissa, Tina"})));
 
         dc.print();
